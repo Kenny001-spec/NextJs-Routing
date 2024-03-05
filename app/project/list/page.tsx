62f22e6a24c6e6681cd047aa465ece0c76e047cc
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-03-05 15:32:04 +0100

    first commit

---
+++ app/project/list/page.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './projects.module.css';
+export default function ProjectList (){
+    return (
+        <main>
+            <h1>ProjectList</h1>
+
+            <ul className={styles.ul}>
+                <li>
+                    <Link href='/projects/jobit'>
+                        Jobit
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/projects/current'>
+                        Car Rent
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/projects/hipnode '>
+                        Hipnode
+                    </Link>
+                </li>
+            </ul>
+        </main>
+    )
+}
